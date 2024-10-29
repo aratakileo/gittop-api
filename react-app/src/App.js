453a4b0f3f92e3ce1b0b2758a1795f77d32219f7
Arataki Leo <83653555+aratakileo@users.noreply.github.com>
2024-10-29 05:58:27 -0700

    React app make basic API call

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { getApiUrlTo } from './utils/constants';
+import { RepoCard } from './components/repo-card';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [repos, setRepos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const processLoading = async () => {
+    setIsLoading(true);
+  
+    await fetch(getApiUrlTo('/v2/repos/page/0'), {
+      method: 'GET',
+      headers: {
+        'Accept': 'aplication/json'
+      }
+    }).then(response => response.json()).then(body => setRepos(body.repos)).catch(setError);
+  
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    processLoading();
+  }, []);
+
+  let body = (<div>Loading...</div>);
+
+  if (error) {
+    console.error(error);
+  }
+
+  if (error)
+    body = (<div>Oh no, something went wrong: {error.toString()}</div>);
+  else if (!isLoading)
+    body = repos.map(repo => <ul key={repo.owner.username + repo.name}>{RepoCard({repo})}</ul>);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body className="App-header">
+        {body}
+      </body>
     </div>
   );
 }
---
+++ react-app/src/components/repo-card.js
@@ -0,0 +1,5 @@
+export const RepoCard = ({repo}) => (<div>
+    Name: {repo.name}
+    <br/>
+    Url: {repo.url}
+</div>);
