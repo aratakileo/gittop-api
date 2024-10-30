bf116aecd4112fedeaa510e9bd280ecbb985ce16
Arataki Leo <83653555+aratakileo@users.noreply.github.com>
2024-10-30 07:51:21 -0700

    Add pagination & styling

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,29 +1,77 @@
-import './App.css';
+import './App.scss';
 import { getApiUrlTo } from './utils/constants';
 import { RepoCard } from './components/repo-card';
 import React, { useState, useEffect } from 'react';
+import { parseDescription, setVisibility } from './utils/utils';
+
+const defaultApiRequestParams = {
+  method: 'GET',
+  headers: {
+    'Accept': 'aplication/json'
+  }
+};
+
+const bufferedPages = {};
 
 function App() {
   const [repos, setRepos] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [hasNextPage, setHasNextPage] = useState(false);
+  const [hasPrevPage, setHasPrevPage] = useState(false);
+
+  const goNextPage = () => {
+    if (hasNextPage)
+      setPage(page + 1);
+  };
+
+  const goPrevPage = () => {
+    if (hasPrevPage)
+      setPage(page - 1);
+  };
+
+  const updateState = () => {
+    setHasPrevPage(page > 0);
+    setHasNextPage(page < totalPages - 1);
+  };
 
   const processLoading = async () => {
+    if (page in bufferedPages) {
+      setRepos(bufferedPages[page]);
+      updateState();
+      return;
+    }
+
     setIsLoading(true);
   
-    await fetch(getApiUrlTo('/v2/repos/page/0'), {
-      method: 'GET',
-      headers: {
-        'Accept': 'aplication/json'
-      }
-    }).then(response => response.json()).then(body => setRepos(body.repos)).catch(setError);
+    await fetch(getApiUrlTo('/v2/repos/page/' + page), defaultApiRequestParams)
+    .then(response => response.json())
+    .then(async body => {
+      for (let repo of body.repos)
+        repo.description = await parseDescription(repo.description);
+
+      bufferedPages[page] = body.repos;
+      setRepos(body.repos);
+    })
+    .catch(setError);
+
+    await fetch(getApiUrlTo('/v2/repos/pages'), defaultApiRequestParams)
+    .then(response => response.json())
+    .then(body => {
+      setTotalPages(body.pages);
+    })
+    .catch(setError);
+
+    updateState();
   
     setIsLoading(false);
   };
 
   useEffect(() => {
     processLoading();
-  }, []);
+  }, [page, totalPages, hasNextPage, hasPrevPage]);
 
   let body = (<div>Loading...</div>);
 
@@ -37,10 +85,15 @@ function App() {
     body = repos.map(repo => <ul key={repo.owner.username + repo.name}>{RepoCard({repo})}</ul>);
 
   return (
-    <div className="App">
-      <body className="App-header">
-        {body}
-      </body>
+    <div className='page-container'>
+      <h1>The most popular GitHub repositories</h1>
+      <div className='repos-container'>
+          {body}
+      </div>
+      <div className='center horizontal'>
+        <button onClick={goPrevPage} className={`${setVisibility(hasPrevPage)} prev-btn`}>Prev</button>
+        <button onClick={goNextPage} className={`${setVisibility(hasNextPage)} next-btn`}>Next</button>
+      </div>
     </div>
   );
 }
--- react-app/src/components/repo-card.js
+++ react-app/src/components/repo-card.js
@@ -1,5 +1,16 @@
-export const RepoCard = ({repo}) => (<div>
-    Name: {repo.name}
-    <br/>
-    Url: {repo.url}
-</div>);
+import { formatedNumericValue, } from "../utils/utils";
+
+export const RepoCard = ({repo}) => {
+    const repoPath = repo.owner.username + '/' + repo.name;
+
+    return (
+    <div className='repo-card bg-hover-animation' onClick={() => window.location.href = repo.url}>
+        <div className='header'>
+            <b>
+                <p className='name'>{repoPath}</p>
+            </b>
+            <div className="stars">{formatedNumericValue(repo.stars)}</div>
+        </div>
+        <p>{repo.description}</p>
+    </div>)
+};
